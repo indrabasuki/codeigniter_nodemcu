a65296f710a1def330a495802da9b16de181ef93
Irvanmaulana35 <irfan.maulan.im47@gmail.com>
2022-07-15 01:47:02 +0700

    update

--- application/views/authentication/login.php
+++ application/views/authentication/login.php
@@ -9,7 +9,7 @@
 	<meta name="author" content />
 	<title>Login</title>
 	<link href="<?= base_url() ?>public/css/styles.css" rel="stylesheet" />
-	<link rel="icon" type="image/x-icon" href="<?= base_url() ?>public/assets/img/favicon.png" />
+	<link rel="icon" type="image/x-icon" href="<?= base_url('public/images/logo.jpeg') ?>" width="50" class="ml-2" />
 	<link rel="stylesheet" href="<?= base_url() ?>public/vendors/fontawesome-free/css/all.min.css" />
 	<script src="<?= base_url() ?>public/vendors/feather-icons/feather.min.js"></script>
 </head>
